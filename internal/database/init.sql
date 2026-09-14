4d6be3f3152f61d87a39e6ff35accbd5cd1c755f
Dithalys <Dithalysdimon99@gmail.com>
2026-09-15 01:16:18 +0300

    add user

--- internal/database/init.sql
+++ internal/database/init.sql
@@ -1,16 +1,25 @@
+CREATE TABLE IF NOT EXISTS Users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    balance NUMERIC(10,2) DEFAULT 0
+);
+
 CREATE TABLE IF NOT EXISTS Components(
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     overview TEXT,
     price NUMERIC(10,2) NOT NULL,
-    quantity INT DEFAULT 0
+    quantity INT DEFAULT 0,
+    User_id INT REFERENCES Users(id)
 );
 
 CREATE TABLE IF NOT EXISTS Computers(
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     overview TEXT,
-    price NUMERIC(10,2) NOT NULL
+    price NUMERIC(10,2) NOT NULL,
+    User_id INT REFERENCES Users(id)
 );
 
 CREATE TABLE IF NOT EXISTS Computer_Components(
