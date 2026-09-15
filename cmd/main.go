8b932498f6a3f95ccd1259278ee170e8edfb244e
Dithalys <Dithalysdimon99@gmail.com>
2026-09-15 15:56:30 +0300

    add shit logs

--- cmd/main.go
+++ cmd/main.go
@@ -8,9 +8,9 @@ import (
 func main() {
 	db := database.Connect()
 	defer db.Close()
-	repository.Add_user(db, "user9", "password9")
+	//repository.Add_user(db, "user1211", "password")
 	//repository.Select_all_users(db)
-	//repository.Delete_user(db, 9)
-	//repository.Update_user(db, "user2", "password2", 7)
-	//repository.Select_all_users(db)
+	//repository.Delete_user(db, 98)
+	//repository.Update_user(db, "user2", "password2", 267)
+	repository.Select_all_users(db)
 }
--- internal/repository/Users_crud.go
+++ internal/repository/Users_crud.go
@@ -4,42 +4,51 @@ import (
 	"PC_Shop/internal/database"
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/lib/pq"
 )
 
 func Add_user(db *sql.DB, name string, password string) {
-	result, err := db.Exec("INSERT INTO Users(username, password_hash) VALUES($1, $2)", name, password)
+	var userID int64
+	query := "INSERT INTO Users(username, password_hash) VALUES($1, $2) RETURNING id"
+	err := db.QueryRow(query, name, password).Scan(&userID)
 	if err != nil {
-		panic(err)
+		log.Println("Ошибка при добавлении поьлзователя:", err)
+	} else {
+		log.Printf("Добавлен пользователь [%d]\n", userID)
 	}
-	fmt.Print(result.RowsAffected())
+
 }
 
 func Delete_user(db *sql.DB, id int) {
-	result, err := db.Exec("DELETE FROM Users WHERE id = $1", id)
+	_, err := db.Exec("DELETE FROM Users WHERE id = $1", id)
 	if err != nil {
-		panic(err)
+		log.Println("Ошибка при удалении поьлзователя:", err)
+	} else {
+		log.Printf("Удаление пользователя [%d]\n", id)
 	}
-	fmt.Print(result.RowsAffected())
 }
 
 func Update_user(db *sql.DB, name string, password string, id int) {
-	result, err := db.Exec("UPDATE Users set username = $1, password_hash = $2 WHERE id = $3", name, password, id)
+	_, err := db.Exec("UPDATE Users set username = $1, password_hash = $2 WHERE id = $3", name, password, id)
 	if err != nil {
-		panic(err)
+		log.Println("Ошибка при обновлении поьлзователя:", err)
+	} else {
+		log.Printf("Обновление пользователя [%d]\n", id)
 	}
-	fmt.Print(result.RowsAffected())
 }
 
 func Select_all_users(db *sql.DB) {
 	rows, err := db.Query("SELECT * FROM Users")
 	if err != nil {
-		panic(err)
+		log.Println("Ошибка при отрисовки поьлзователей:", err)
+	} else {
+		log.Printf("Отрисовка списка пользователей")
 	}
 	defer rows.Close()
-	users := []database.User{}
 
+	users := []database.User{}
 	for rows.Next() {
 		user := database.User{}
 		err := rows.Scan(&user.Id, &user.Username, &user.Password_hash, &user.Balance)
@@ -53,3 +62,5 @@ func Select_all_users(db *sql.DB) {
 		fmt.Println(u.Id, u.Username, u.Password_hash, u.Balance)
 	}
 }
+
+// сделать возврат результата функциям
